831276070ec71bdd2b05ed30d939e376c893efe3
Đặng Ngọc Thái <140839033+mr-thai@users.noreply.github.com>
2024-12-06 17:58:17 +0700

    Refactor about, contact, home, project, and footer pages: update styles for improved aesthetics and responsiveness; enhance button interactions and background gradients

--- assets/js/signup.js
+++ assets/js/signup.js
@@ -2,9 +2,11 @@ function showSignupToast(isSuccess) {
   const toastId = isSuccess ? "signupSuccessToast" : "signupErrorToast";
   const toast = new bootstrap.Toast(document.getElementById(toastId));
   toast.show();
-  setTimeout(function () {
-    window.location.href = "login.html";
-  }, 3000);
+  if (isSuccess) {
+    setTimeout(function () {
+      window.location.href = "login.html";
+    }, 3000);
+  }
 }
 document
   .getElementById("signupForm")
